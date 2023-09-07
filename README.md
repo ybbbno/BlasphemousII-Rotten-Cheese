4196cf8a03c94ee60ea445e5cdd9ba3f50b77e08
ybbbno <iterfly99@gmail.com>
2023-09-07 11:05:58 +0700

    Try to create a loader for textures from game

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Mod for Blasphemous II that changes Flasks to Rotten Cheese (in progress)
+Mod for Blasphemous 2 that changes Flasks to Rotten Cheese (in progress)
 
 If you want play with this mod, you need to install Modding Tools
 https://github.com/BrandenEK/BlasII.ModdingTools
--- rotten_cheese/rotten_cheese.cs
+++ rotten_cheese/rotten_cheese.cs
@@ -1,10 +1,9 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
 using BlasII.ModdingAPI;
 using Il2Cpp;
-using Il2CppTGK.Game;
-using Il2CppTGK.Game.Components.StatsSystem;
 using UnityEngine;
-
 namespace rotten_cheese
 {
     public class rotten_cheese : BlasIIMod
@@ -15,42 +14,127 @@ namespace rotten_cheese
         {
             LogError($"{ModInfo.MOD_NAME} is initialized");
         }
+        Texture2D duplicateTexture(Texture2D source)
+        {
+            RenderTexture renderTex = RenderTexture.GetTemporary(
+                        source.width,
+                        source.height,
+                        0,
+                        RenderTextureFormat.Default,
+                        RenderTextureReadWrite.Linear);
+
+            Graphics.Blit(source, renderTex);
+            RenderTexture previous = RenderTexture.active;
+            RenderTexture.active = renderTex;
+            Texture2D readableText = new Texture2D(source.width, source.height);
+            readableText.ReadPixels(new Rect(0, 0, renderTex.width, renderTex.height), 0, 0);
+            readableText.Apply();
+            RenderTexture.active = previous;
+            RenderTexture.ReleaseTemporary(renderTex);
+            readableText.name = source.name;
+            return readableText;
+        }
+
         protected override void OnSceneLoaded(string sceneName)
         {
+            GameObject ingame_flask_object = GameObject.Find("Flask");
+            Log("Found Flask GameObject: " + ingame_flask_object.ToString());
+            //Log(ingame_flask_object.GetType().GetMethods());    
+            UIFlaskBar ingame_flask_ui_bar = ingame_flask_object.GetComponent<UIFlaskBar>();
+            Log("Found Flask UIFlaskBar: " + ingame_flask_ui_bar.ToString());
+
+            Main.rotten_cheese.FileHandler.LoadDataAsSprite("cheese.png", 24, new Vector2(0, 0), true, out Sprite new_sprite);
+            Log("New Flask Sprite was created:" + new_sprite.ToString());
+            int i = 0;
+            foreach(var ingame_child_flask in ingame_flask_ui_bar.childFlask){
+                i++;
+                for(int j = 0; j < ingame_child_flask.upgradeImagesFill.Count; j++){
+                    if(i == 1){
+                        var ingame_child_flask_sprite = ingame_child_flask.upgradeImagesFill[j];
+                        var ingame_child_flask_texture = ingame_child_flask_sprite.texture;
+                        var child_flask_texture = duplicateTexture(ingame_child_flask_texture);
+                        Log(child_flask_texture.name+".png="+child_flask_texture.GetPixels());
+                        byte[] child_flask_texture_bytes = child_flask_texture.EncodeToPNG();
+                        File.WriteAllBytes("Modding/data/"+child_flask_texture.name+j+"Fill.png", child_flask_texture_bytes);
+                    }
+                    //ingame_child_flask.upgradeImagesFill[j] = new_sprite;
+                }
+                for(int j = 0; j < ingame_child_flask.upgradeImagesEmpty.Count; j++){
+                    if(i == 1){
+                        var ingame_child_flask_sprite = ingame_child_flask.upgradeImagesEmpty[j];
+                        var ingame_child_flask_texture = ingame_child_flask_sprite.texture;
+                        var child_flask_texture = duplicateTexture(ingame_child_flask_texture);
+                        Log(child_flask_texture.name+".png="+child_flask_texture.GetPixels());
+                        byte[] child_flask_texture_bytes = child_flask_texture.EncodeToPNG();
+                        File.WriteAllBytes("Modding/data/"+child_flask_texture.name+j+"Empty.png", child_flask_texture_bytes);
+                    }
+                    //ingame_child_flask.upgradeImagesEmpty[j] = new_sprite;
+                }
+            }
+
+            Log("All sprites was successfully modified");
+
+            /*
             GameObject flask_object = GameObject.Find("Flask");
             Log("Found Flask GameObject: " + flask_object.ToString());
 
             UIFlaskBar flask_ui_bar = flask_object.GetComponent<UIFlaskBar>();
             Log("Found Flask UIFlaskBar: " + flask_ui_bar.ToString());
-            // stat - Flask
-            // flaskStrengthStat - Healing Flasks Factor
-            // statRef - PlayerStatsComponent Ref (TGK.Game.Components.StatsSystem.StatsComponentRef)
-            // childFlask - Il2CppInterop.Runtime.InteropTypes.Arrays.Il2CppReferenceArray`1[Il2Cpp.UIFlaskUpgradeControl]
-            // staticControl - False
-            // noFlaskWarningFade - FlaskWarning (TGK.Game.Components.FadeImage)
-            // noFlaskWarning - FlaskWarning (UIFlaskUpgradeControl)
-            // delayBeforeFillPerFlask - 0,2
-            // statsComponent - #Main Player (TGK.Game.Components.StatsSystem.StatsComponent)
-            // player - #Main Player (Lightbug.Kinematic2D.Implementation.CharacterController2D)
-            // flaskUse - abilities (Game._3rd_party.Kinematic2D.Implementation.Scripts.Character_Abilities.Item_Usage.FlaskUse)
-            // prevLastFilledFlask - 3
 
             Main.rotten_cheese.FileHandler.LoadDataAsSprite("cheese.png", 23, new Vector2(0, 0), true, out Sprite rooten_cheese_sprite);
 
             Log("Rotten Cheese Sprite was created:" + rooten_cheese_sprite.ToString());
-
-            foreach(var child_flask in flask_ui_bar.childFlask){
+            */
+            //foreach(var child_flask in flask_ui_bar.childFlask){
                 /*
                 foreach(var empty in child_flask.upgradeImagesEmpty){
                     Log(empty.ToString());
                 }
                 */
-                for(int i = 0; i < child_flask.upgradeImagesFill.Count(); i++){
-                    child_flask.upgradeImagesFill[i] = rooten_cheese_sprite;
-                }
-            }
+                //for(int j = 0; j < child_flask.upgradeImagesFill.Count(); j++){
+                //    child_flask.upgradeImagesFill[j] = rooten_cheese_sprite;
+                //}
 
-            Log("All sprites was successfully modified");
+                /*
+                if (i <= 3){
+                    Log("\nNumber of flusk is "+i);
+                    
+                    string upgradeImagesEmpty_str = null;
+                    if (child_flask.upgradeImagesEmpty != null) {
+                        upgradeImagesEmpty_str = "";
+                        foreach (var image_empty in child_flask.upgradeImagesEmpty) upgradeImagesEmpty_str += image_empty.ToString()+", ";
+                    }
+                    Log("upgradeImagesEmpty: "+upgradeImagesEmpty_str);
+                    
+                    string upgradeImagesFill_str = null;
+                    if (child_flask.upgradeImagesFill != null) {
+                        upgradeImagesFill_str = "";
+                        foreach (var image_empty in child_flask.upgradeImagesFill) upgradeImagesFill_str += image_empty.ToString()+", ";
+                    }
+                    Log("upgradeImagesFill: "+upgradeImagesFill_str);
+                    
+                    //UnityEngine.UI.Image
+                    var imageControl = child_flask.imageControl != null ? child_flask.imageControl.sprite.ToString() : null;
+                    Log("imageControl: "+imageControl);
+
+                    Log("animDelay: "+child_flask.animDelay);
+
+                    var OnFilled = child_flask.OnFilled != null ? child_flask.OnFilled.ToString() : null;
+                    Log("OnFilled: "+OnFilled);
+
+                    var currentSprite = child_flask.currentSprite != null ? child_flask.currentSprite.ToString() : null;
+                    Log("currentSprite: "+currentSprite);
+
+                    Log("prevUpgradeLevel: "+child_flask.prevUpgradeLevel);
+
+                    Log("wasFilled: "+child_flask.wasFilled);
+
+                    Log("delayBeforeFill: "+child_flask.delayBeforeFill);
+                }
+                */
+
+
+            //Log("All sprites was successfully modified");
             
             /*
             Il2CppTGK.Game.Components.FadeImage flask_warning_fade = flask_ui_bar.noFlaskWarningFade;
@@ -86,20 +170,11 @@ namespace rotten_cheese
                     //Component[] components = flask_object.GetComponents();
                     
                 }
+            
             }
-            */
+            
+           */ 
         }
-
-        private StatsComponent _playerStats;
-        public StatsComponent PlayerStats
-        {
-            get
-            {
-                if (_playerStats == null)
-                    _playerStats = CoreCache.PlayerSpawn.PlayerInstance.GetComponent<StatsComponent>();
-                return _playerStats;
-            }
-        }
-
+        
     }
 }
